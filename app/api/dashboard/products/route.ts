ce122dfea5c7f10d0962607b861978f6c981e760
Guilherme Nunes <guilherme.augd@gmail.com>
2025-10-29 20:42:00 -0300

    implemented post method handling logic

--- app/api/dashboard/products/route.ts
+++ app/api/dashboard/products/route.ts
@@ -11,3 +11,23 @@ export async function GET() {
     return NextResponse.json({error:'Failed to fetch products'}, { status: 500 });
   }
 }
+
+
+export async function POST(request: Request) {
+  try {
+    const body = await request.json();
+    const response = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/api/produtos`, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(body),
+    });
+    const data = await response.json();
+    return NextResponse.json(data);
+
+  } catch (error) {
+    console.error('Error fetching products:', error);
+    return NextResponse.json({error:'Failed to fetch products'}, { status: 500 });
+  }
+}
