3621e5f0582a6415093680c0b4ef729b54144bfb
Guilherme Nunes <guilherme.augd@gmail.com>
2025-10-30 10:14:54 -0300

    implement product card display logic

--- app/components/ProductCard.tsx
+++ app/components/ProductCard.tsx
@@ -6,11 +6,11 @@ interface ProdutoCardProps {
   produto: Produto;
 }
 
-export default function ProdutoCard({ produto }: ProdutoCardProps) {
+export default function ProductCard({ produto }: ProdutoCardProps) {
   return (
-    <Link href={`/ produtos / ${ produto.id }`} className="bg-[#31394c] border border-gray-200 rounded-lg p-6 shadow-md hover:shadow-xl hover:-translate-y-1 transition-all duration-300">
-      <h2 className="text-2xl font-semibold text-gray-900 truncate">{produto.nome}</h2>
-      <p className="text-xl text-blue-600 font-medium mt-2">
+    <Link href={`/ produtos / ${ produto.id }`} className="bg-[#31394c] border border-gray-700 rounded-lg p-6 py-7 shadow-md hover:shadow-xl hover:-translate-y-1 transition-all duration-300">
+      <h2 className="text-2xl font-semibold text-white truncate">{produto.nome}</h2>
+      <p className="text-xl text-amber-500 font-medium mt-2">
         {new Intl.NumberFormat('en-US', {
           style: 'currency',
           currency: 'USD',
